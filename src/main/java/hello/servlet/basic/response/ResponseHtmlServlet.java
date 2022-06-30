d0062446c2f173e623ee8d0bdf52a1af7927b900
KDH_Notebook <destiny1017@nate.com>
2022-06-30 14:05:35 +0900

    HTTP 응답 데이터

---
+++ src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHtmlServlet", urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html");
+
+        PrintWriter writer = resp.getWriter();
+        writer.write("<html>");
+        writer.write("<body>");
+        writer.write("<div>헬로</div>");
+        writer.write("</body>");
+        writer.write("</html>");
+    }
+}
